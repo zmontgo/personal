b9cdb237d4a605dbaa5da2585052ab6d09dfa7ed
zmontgo <31627113+zmontgo@users.noreply.github.com>
2021-12-31 08:32:05 -0600

    Remoe unneeded files

--- dist/routes/static-pages.js
+++ dist/routes/static-pages.js
@@ -4,120 +4,14 @@ exports.init = void 0;
 const router_1 = require("../components/router");
 function init() {
     router_1.router.get("/", async (ctx) => {
+        console.log('Hi');
         ctx.render("index.pug", {
-            title: "Home | North Point Heating and Air",
+            title: "Home | Zachary Montgomery",
             heroClass: true,
         });
     });
-    router_1.router.get("/about", async (ctx) => {
-        ctx.render("about.pug", {
-            title: "About | North Point Heating and Air",
-        });
-    });
-    router_1.router.get("/reviews", async (ctx) => {
-        ctx.render("reviews.pug", {
-            title: "Reviews | North Point Heating and Air",
-        });
-    });
-    router_1.router.get("/air-conditioning", async (ctx) => {
-        ctx.render("services/air-conditioning.pug", {
-            title: "Air Conditioning | North Point Heating and Air",
-        });
-    });
-    router_1.router.get("/heating", async (ctx) => {
-        ctx.render("services/heating.pug", {
-            title: "Heating | North Point Heating and Air",
-        });
-    });
-    router_1.router.get("/air-quality", async (ctx) => {
-        ctx.render("services/air-quality.pug", {
-            title: "Air Quality | North Point Heating and Air",
-        });
-    });
-    router_1.router.get("/weatherization", async (ctx) => {
-        ctx.render("services/weatherization.pug", {
-            title: "Weatherization | North Point Heating and Air",
-        });
-    });
-    router_1.router.get("/schedule", async (ctx) => {
-        ctx.render("schedule.pug", {
-            title: "Schedule Service | North Point Heating and Air",
-        });
-    });
-    router_1.router.get("/privacy", async (ctx) => {
-        ctx.render("privacy.pug", { title: "Privacy Policy | North Point Heating and Air" });
-    });
-    router_1.router.get("/robots.txt", async (ctx) => {
-        ctx.render("robots.pug");
-    });
-    router_1.router.get("/sitemap.xml", async (ctx) => {
-        ctx.type = 'text/xml';
-        ctx.body = `<urlse>
-    <!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
-    <url>
-    <loc>http://104.131.24.42/</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>1.00</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/air-conditioning</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/heating</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/air-quality</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/weatherization</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/about/</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/reviews/</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/contact/</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/contact</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/privacy</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/feedback</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/not-a-page</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.64</priority>
-    </url>
-    </urlset>`;
-    });
     router_1.router.get("/humans.txt", async (ctx) => {
-        ctx.body = "Made with ❤️ by Zachary Montgomery for North Point Heating and Air.";
+        ctx.body = "Made with ❤️ by Zachary Montgomery.";
     });
 }
 exports.init = init;
--- src/routes/static-pages.ts
+++ src/routes/static-pages.ts
@@ -1,133 +1,16 @@
 import { router } from "../components/router";
-import { PublicError } from "../components/sentry";
-import * as fs from "fs";
 
 export function init() {
   router.get("/", async (ctx) => {
+    console.log('Hi');
+
     ctx.render("index.pug", {
-      title: "Home | North Point Heating and Air",
+      title: "Home | Zachary Montgomery",
       heroClass: true,
     });
   });
 
-  router.get("/about", async (ctx) => {
-    ctx.render("about.pug", {
-      title: "About | North Point Heating and Air",
-    });
-  });
-
-  router.get("/reviews", async (ctx) => {
-    ctx.render("reviews.pug", {
-      title: "Reviews | North Point Heating and Air",
-    });
-  });
-
-  router.get("/air-conditioning", async (ctx) => {
-    ctx.render("services/air-conditioning.pug", {
-      title: "Air Conditioning | North Point Heating and Air",
-    });
-  });
-
-  router.get("/heating", async (ctx) => {
-    ctx.render("services/heating.pug", {
-      title: "Heating | North Point Heating and Air",
-    });
-  });
-  
-  router.get("/air-quality", async (ctx) => {
-    ctx.render("services/air-quality.pug", {
-      title: "Air Quality | North Point Heating and Air",
-    });
-  });
-
-  router.get("/weatherization", async (ctx) => {
-    ctx.render("services/weatherization.pug", {
-      title: "Weatherization | North Point Heating and Air",
-    });
-  });
-
-  router.get("/schedule", async (ctx) => {
-    ctx.render("schedule.pug", {
-      title: "Schedule Service | North Point Heating and Air",
-    });
-  });
-
-  router.get("/privacy", async (ctx) => {
-    ctx.render("privacy.pug", {title: "Privacy Policy | North Point Heating and Air"});
-  });
-
-  router.get("/robots.txt", async (ctx) => {
-    ctx.render("robots.pug");
-  });
-
-  router.get("/sitemap.xml", async (ctx) => {
-    ctx.type = 'text/xml'
-    ctx.body = `<urlse>
-    <!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
-    <url>
-    <loc>http://104.131.24.42/</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>1.00</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/air-conditioning</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/heating</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/air-quality</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/weatherization</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/about/</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/reviews/</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/contact/</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/contact</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/privacy</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/feedback</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.80</priority>
-    </url>
-    <url>
-    <loc>http://104.131.24.42/not-a-page</loc>
-    <lastmod>2021-11-22T21:21:11+00:00</lastmod>
-    <priority>0.64</priority>
-    </url>
-    </urlset>`
-  })
-
   router.get("/humans.txt", async (ctx) => {
-    ctx.body = "Made with ❤️ by Zachary Montgomery for North Point Heating and Air."
+    ctx.body = "Made with ❤️ by Zachary Montgomery."
   })
 }
